9ed5cbbb59bb45dd7cedc74ac85bbd4502d86fd3
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-19 13:22:12 +0000

    File modification

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,17 +1,16 @@
 /**
-* Use
-* of
-* printf
-* function
-*/
+ * main - Prints \"with proper grammar, but the outcome is a piece of art,
+ * Return: Always 0.
+ */
 
 #include <stdio.h>
 
-/* Display some text with the function printf */
-
 int main(void)
+
+/* main -  Display some text with the function printf */
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
