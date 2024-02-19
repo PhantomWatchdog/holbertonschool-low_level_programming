e648facd9d09aac8d1c4f9a1df5af21a08e078b4
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-19 13:05:32 +0000

    Update

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/**
+* Use
+* of
+* printf
+* function
+*/
+
+#include <stdio.h>
+
+/* Display some text with the function printf */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
