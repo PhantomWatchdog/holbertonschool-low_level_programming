bbbf9765d556ea8161f11dd0159cc5c480979d5b
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-19 12:33:16 +0000

    File modification

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main(void) 
+
+int main(void)
+
 {
-  int test =  puts("\"Programming is like building a multilingual puzzle");
-   return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
