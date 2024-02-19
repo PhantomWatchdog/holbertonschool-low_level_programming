bc19b29f57fa85223dc63c9cf517c42ddb1eedde
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-19 13:47:57 +0000

    File creation

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+/**
+ * main - Print the size of various types on computer
+ * Return: Always 0.
+ */
+
+#include <stdio.h>
+
+int main(void)
+
+/* main - Size is not grandeur, and territory does not make a nation */
+
+{
+	printf("Size of a char: %lu byte(s)";sizeof(char));
+	printf("Size of an int: %lu byte(s)";sizeof(int));
+	printf("Size of a long int: %lu byte(s)";sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)";sizeof(long long int));
+	printf("Size of a float: %lu byte(s)";syzeof(float));
+
+	return (0);
+}
