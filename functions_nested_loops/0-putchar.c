8f2022834cdd49527a29d8e1d60347f5fe637e95
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-20 09:54:01 +0000

    File creation

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the _putchar term
+ * Return: zero
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
