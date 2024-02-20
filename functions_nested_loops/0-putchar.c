3493a4b7c99c1e287fa3efb188bb4186c997fac8
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-20 11:31:44 +0000

    Update

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,15 +7,10 @@
 
 int main(void)
 {
-	int main(char c)
-	{
-		_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		
-		return (fwrite(1, &c, 1));
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
 	
 	return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 /* definition of putchar prototype */
 
 int _putchar(char c);
