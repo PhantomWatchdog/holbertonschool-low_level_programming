9fd4e3b91aa0a48ce7683b17d3e5803fe42ef81b
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-20 19:49:44 +0000

    File creation

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - check each integer passed
+ * and return 1 if lowercase
+ * or 0 if not,
+ * with a new line
+ * Return: zero
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
