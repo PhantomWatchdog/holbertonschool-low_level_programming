02bdd50195f6db9122341c9b207c91803d0f7e8a
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-20 20:02:34 +0000

    Betty style update

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _islower - check each integer passed
- * and return 1 if lowercase
- * or 0 if not,
- * with a new line
- * Return: zero
+ * _islower - check each character for control his lower state or not.
+ * @c: is the character to check, here between a and z.
+ * Return: 1 if lowercase or 0 if not.
 */
 
 int _islower(int c)
