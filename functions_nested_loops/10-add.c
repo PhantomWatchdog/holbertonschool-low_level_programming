ce0c192f03ebee77a2016d6e25b109eae628644f
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-21 23:29:43 +0000

    Function add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - two var declaration,
+ * @var1: contain first integer,
+ *
+ * @var2: contain second integer,
+ * Return: sum.
+*/
+
+int add(int var1, int var2)
+{
+	return (var1 + var2);
+}
