bc73f600635366e01576b8e3a1e329e2ae5c88b2
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-23 09:10:44 +0000

    All two digits combinations

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints two digits from 0 to 9 separated by ' ' and ','
+ * and displaying all digits combinations
+ * Return: zero
+*/
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar((num1 + '0'));
+				putchar((num2 + '0'));
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
