4c66366e144dec786c0aea271f45d22e5e606376
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-23 15:06:48 +0000

    Print n times table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - for a determined value,
+ * display the result in order until specified limit,
+ * @n: is the var who contain the value,
+ * with a new line at each start,
+ * don't display results if n is greater than 15 or less than 0,
+ * Return: zero
+*/
+
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		int num1, num2;
+
+		for (num1 = 0; num1 <= n; num1++)
+		{
+			for (num2 = 0; num2 <= n; num2++)
+			{
+				int result = num1 * num2;
+
+				printf("%-4d", result);
+			}
+
+			printf("\n");
+
+		}
+	}
+
+}
