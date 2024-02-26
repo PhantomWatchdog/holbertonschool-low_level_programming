15ea4e284db03e3335c98858400b2d0521d00094
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-26 15:31:07 +0000

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the digit number from 0 to 100,
+ * followed by a '\n',
+ * for each multiples of 3 display Fizz,
+ * multiples of 5 must display Buzz,
+ * and for multiples of 3 and 5 display FizzBuzz,
+ * Return: zero
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		else
+		{
+			printf("%d ", i);
+		}
+
+	}
+
+	printf("\n");
+	return (0);
+
+}
