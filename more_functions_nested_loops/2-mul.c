1556eb2841551f6c2092ca8744a9c7f33c0f9240
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-26 10:27:09 +0000

    Multiplication function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies towo integers.
+ * @a: is the first factor to mutiplie,
+ * @b: is the second factor to multiplie,
+ * Return: zero.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
