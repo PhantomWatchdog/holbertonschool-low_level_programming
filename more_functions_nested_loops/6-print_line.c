b455343ca1bfda13dfe51b574e0d8f4d723e5c81
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-26 12:24:59 +0000

    File creation

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - print x times '_' due to n value,
+ * @n: contain the x times value required,
+ * i: number of incrementation due to n value,
+ * new line as break if n = 0,
+ * Return: zero
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
