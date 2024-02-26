212e51ae324dcedd16055c7f393051b1518efde8
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-26 12:44:58 +0000

    File updated

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,23 +10,19 @@
 
 void print_line(int n)
 {
-	int i;
-
-	if (n < 0)
+	if (n <= 0)
 	{
-		_putchar('$');
 		_putchar('\n');
 	}
 
 	else
 	{
+		int i;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
-
-		_putchar('$');
 	}
 
 	_putchar('\n');
