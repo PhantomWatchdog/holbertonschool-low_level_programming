7af1a6bbe4a80d943629670f07fd66b47d835fa1
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-26 10:12:28 +0000

    Update to upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - check each character for control his lower state or not.
- * @c: is the character to check, here between a and z.
+ * @c: is the character to check, here between A and Z.
  * Return: 1 if lowercase or 0 if not.
 */
 
