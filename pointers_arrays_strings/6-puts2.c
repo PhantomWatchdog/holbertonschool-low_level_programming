3e4957cd9e93744ee01a5dc4f8e6c3ea6a31769f
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-28 12:46:15 +0000

    Puts V2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - display selected chars in *str,
+ * @str: contain the char*
+ * i: incrementation by 2,
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
