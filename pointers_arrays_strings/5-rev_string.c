cd9f5882c305feed9690659f99dde230a8d74934
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-28 12:01:14 +0000

    Reverse a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - function used for swap value of s,
+ * start: first var used for the swap ,
+ * end: second var used for the swap,
+ * first loop move pointer until last char of *s \0,
+ * with one decrementation to char before \0,
+ * the second move pointer until first char of *s \0,
+ * in reverse, decrement to char after 'M',
+ * Return: zero.
+ */
+
+void rev_string(char *s)
+{
+	char *start = s;
+	char *end = s;
+	
+	while (*end != '\0') {
+		end++;
+	}
+	
+	end--;
+	
+	while (start < end)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
+}
