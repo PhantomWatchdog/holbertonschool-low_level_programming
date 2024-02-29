d94e5fab41f76cc29e945374c193cf7c41680361
PhantomWatchdog <8706@holbertonstudents.com>
2024-02-29 15:48:08 +0000

    Copy and return string pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - function swaping string with two parameters,
+ * @dest: will receive the string containded by src;
+ * @src: contain the string to copy,
+ * Return: dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
