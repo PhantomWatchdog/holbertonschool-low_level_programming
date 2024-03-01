9cf840732852ae7a91fa8c02b7a2e142f5eccaf2
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-01 07:48:26 +0000

    NULL testing

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index] != '\0')
+	while (src[index])
 	{
 		dest[index] = src[index];
 		index++;
 	}
+	dest = '\0';
+
 
 	return (dest);
 }
