21645cf7979424106ca4f0e6ff3a362d35c3d426
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-02 11:45:11 +0000

    betty fix

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat - function used for concatenation,
+ * _strncat - function used for two str concatenation,
  * @dest: destination,
  * @src: source,
- * @n: 
+ * @n: parameter for n char add at end of string,
  * Return: dest.
  */
 
 /* function declaration with 3 pointer, 3 parameters (*dest, *src, n) */
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_length = 0;
-    int index = 0;
+	int dest_length = 0;
+	int index = 0;
 
 /* determine the dest lenght chain */
-    while (dest[index++])
-        dest_length++;
+	while (dest[index++])
+		dest_length++;
 
 /* concatenates the src chain to dest chain with max n added */
-    for (index = 0; src[index] && index < n; index++)
-        dest[dest_length++] = src[index];
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_length++] = src[index];
 
-    return dest;
+	return (dest);
 }
 
