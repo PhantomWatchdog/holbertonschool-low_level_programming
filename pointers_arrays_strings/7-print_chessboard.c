47e4c72bb3e74c134766df07ddf9489b0133de9f
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-05 11:00:47 +0000

    chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_chessboard - display the chessboard acording to *a,
+ * @a: pointer to board,
+ * Return: each char from chessboard.
  */
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	for (i = 0; i < '\0'; i++)
+	int index, jndex;
+/* loop moving pointer in a[], index (row), jndex (column) */
+	for (index = 0; a[index][7]; index++)
 	{
-		for (j = 0; j < 8; j++)
+		for (jndex = 0; jndex < 8; jndex++)
 		{
-			_putchar(a[i][j]);
+			_putchar(a[index][jndex]);
 		}
 
+		_putchar('\n');
+	}
 
+}
