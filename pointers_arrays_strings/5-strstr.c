f5052536f9a4896b854927b0699baf3f579f31a0
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-05 11:07:29 +0000

    Code update

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,6 +11,11 @@ char *_strstr(char *haystack, char *needle)
 {
 	int index;
 
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
 	while (*haystack)
 	{
 		for (index = 0; needle[index] && needle[index] == haystack[index]; index++)
