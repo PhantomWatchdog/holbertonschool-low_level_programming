63f6fd3115c1d303efc7f3d6cfc6f3eae61bedbf
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-08 14:01:16 +0000

    Print args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print the program name.
+ * @argc: The number of arguments in argv array.
+ * @argv: The array containing all arguments.
+ * Return: Always return program name  contained in argv[0].
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
