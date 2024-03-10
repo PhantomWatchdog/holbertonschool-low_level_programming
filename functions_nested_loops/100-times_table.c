0649424c328320e7d6c3b7ce51a051d87574fef3
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-10 17:18:07 +0000

    Print n times table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -5,28 +5,49 @@
  * print_times_table - for a determined value,
  * display the result in order until specified limit,
  * @n: is the var who contain the value,
- * with a new line at each start,
  * don't display results if n is greater than 15 or less than 0,
  * Return: zero
 */
 
 void print_times_table(int n)
 {
+	int num, mult, prod;
+
 	if (n >= 0 && n <= 15)
 	{
-		int num1, num2;
-
-		for (num1 = 0; num1 <= n; num1++)
+		for (num = 0; num <= n; num++)
 		{
-			for (num2 = 0; num2 <= n; num2++)
-			{
+			_putchar('0');
 
-				printf("%-4d", num1 * num2);
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					/* Print the hundreds + tens + units */
+					_putchar(prod / 100 + '0');
+					_putchar(prod / 10 % 10 + '0');
+				}
+
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar(prod / 10 + '0');
+				}
+
+				_putchar(prod % 10 + '0');
 			}
 
-			printf("\n");
-
+			_putchar('\n');
 		}
 	}
-
 }
