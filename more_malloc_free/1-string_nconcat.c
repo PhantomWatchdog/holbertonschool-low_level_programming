3abfb7287f09b2ca9dfb6629c25921e9b180d409
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-12 11:49:09 +0000

    Code achitecture change

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,73 +3,6 @@
 #include <string.h>
 
 /**
- * * _strlen - Check string length.
- * @s: Parameter to s string.
- * ptr: Pointer to the s value;
- * Return: length.
- */
-
-int _strlen(char *s)
-{
-	char *ptr = s;
-
-	while (*ptr != '\0')
-	{
-		ptr++;
-	}
-
-	return (ptr - s);
-
-}
-
-/**
- * _strcpy - Copy src string to dest string.
- * @dest: Receive the string containded by src;
- * @src: Contain the string to copy,
- * Return: dest.
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int index = 0;
-
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
-	if (dest[index])
-		dest[index] = '\0';
-
-	return (dest);
-}
-
-/**
- * _strncat - Concatenation of two str.
- * @dest: Destination,
- * @src: Source,
- * @n: Add n char at end of string,
- * Return: dest.
- */
-
-char *_strncat(char *dest, char *src, int n)
-{
-	int dest_length = 0;
-	int index = 0;
-
-/* determine the dest lenght chain */
-	while (dest[index++])
-		dest_length++;
-
-/* concatenates the src chain to dest chain with max n added */
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_length++] = src[index];
-
-	return (dest);
-}
-
-/**
  * string_nconcat - Concatenate two strings.
  * @s1: First string.
  * @s2: Second string.
@@ -82,36 +15,33 @@ char *_strncat(char *dest, char *src, int n)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int length1 = 0, length2 = 0, total;
+	unsigned int length = n, index;
 	char *alloc;
 
 	/* If s1 or s2 is NULL, consider as empty */
 	s1 = (s1 == NULL) ? "" : s1;
 	s2 = (s2 == NULL) ? "" : s2;
 
-	/* Calcul the s1 and s2 length */
-	length1 = _strlen(s1);
-	length2 = _strlen(s2);
-
-	/* If n >= length2, use s2 length */
-	n = (n >= length2) ? length2 : n;
-
 	/* Calcul the total length */
-	total = length1 + n;
+	for (index = 0; s1[index]; index++)
+		length++;
 
-	alloc = malloc((total + 1) * sizeof(char));
+	alloc = malloc((length + 1) * sizeof(char));
 
 	/* Check if malloc succeed */
 	alloc = (alloc == NULL) ? NULL : alloc;
 
+	length = 0;
 	/* Copy s1 in alloc */
-	_strcpy(alloc, s1);
+	for (index = 0; s1[index]; index++)
+		alloc[length++] = s1[index];
 
-	/* Concatenates s2 first n bytes to alloc */
-	_strncat(alloc, s2, n);
+	/* concatenation of n char from s2 to alloc */
+	for (index = 0; s2[index] && index < n; index++)
+		alloc[length++] = s2[index];
 
 	/* End string with NULL */
-	alloc[total] = '\0';
+	alloc[length] = '\0';
 
 	return (alloc);
 }
