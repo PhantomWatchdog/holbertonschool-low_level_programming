5c895ddf72c15f98cc2cc9ed1b4a75ec682a00cc
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-14 14:35:08 +0000

    Fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -78,7 +78,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	rex->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (rex->owner == NULL)
 	{
-		free(rex->name);
 		free(rex);
 		return (NULL);
 	}
