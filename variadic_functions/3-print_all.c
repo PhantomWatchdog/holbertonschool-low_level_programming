9f371825c84c53703fe8dcb980f7ba129e6669ba
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-20 22:27:18 +0000

    Print anything with va_func

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,7 +2,7 @@
 
 /**
  * print_char - Prints a char.
- *@arg: Arguments to prints.
+ *@args: Arguments to prints.
  *Return: letter.
  */
 
@@ -18,7 +18,7 @@ void print_char(va_list args)
 
 /**
  * print_int - Prints a integer.
- *@arg: Arguments to prints.
+ *@args: Arguments to prints.
  *Return: num.
  */
 
@@ -32,7 +32,7 @@ void print_int(va_list args)
 
 /**
  * print_float - Prints a float.
- *@arg: Arguments to prints.
+ *@args: Arguments to prints.
  *Return: num.
  */
 
@@ -46,7 +46,7 @@ void print_float(va_list args)
 
 /**
  * print_string - Prints a string.
- *@arg: Arguments to prints.
+ *@args: Arguments to prints.
  *Return: str.
  */
 
@@ -55,54 +55,61 @@ void print_string(va_list args)
 	char *str;
 
 	str = va_arg(args, char *);
+
 	if (str == NULL)
+	{
 		printf("(nil)");
-	else
-		printf("%s", str);
+	}
+
+	printf("%s", str);
 }
 
 /**
  * print_all - Prints all arguments.
- *@format: A string of chars.
+ *@format: List of args types.
  *Return: args.
  */
 
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
+	int i, j;
 	char *separator = "";
 
+	/* Use an struct for find respective type and f() */
+	print_type f[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	/* Init args list */
 	va_start(args, format);
 
+	j = 0;
+
 	/* Check each arg in format and print it */
-	while (format && format[i])
+	while (format != NULL && format[j] != '\0')
 	{
-		if (format[i] == 'c')
+		i = 0;
+		/* Search the type for each arg */
+		while (f[i].letter != '\0')
 		{
-			printf("%s", separator);
-			print_char(args);
+			/* Check if type is matching */
+			if (f[i].letter == format[j])
+			{
+				printf("%s", separator);
+				f[i].print(args);
+				separator = ", ";
+			}
+			/* Move on types */
+			i++;
 		}
-		else if (format[i] == 'i')
-		{
-			printf("%s", separator);
-			print_int(args);
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s", separator);
-			print_float(args);
-		}
-		else if (format[i] == 's')
-		{
-			printf("%s", separator);
-			print_string(args);
-		}
-
-		separator = ", ";
-		i++;
+		/* Move on chars */
+		j++;
 	}
-
 	va_end(args);
 	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+typedef struct type
+{
+	char letter;
+	void (*print)(va_list args);
+} print_type;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
