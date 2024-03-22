a20dc4f398de99eb3989094ad4e9667f0b0bb1e4
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-22 22:18:42 +0000

    Deletion of comments

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,13 +1,14 @@
-global  main	;Declaration of main function as global symbol
-section .text	;Section containing the executable part of the program
-main:		;Start of main function
-        mov     rax, 4	;Load the numero system call for sys_write in rax register
-        mov     rbx, 1	;Load the numero of file in rbx register (stdout)
-        mov     rcx, message	;Load & of messagein rcx register
-        mov     rdx, 17	;Load the length of message in rdx register
-        int	0x80	;System call of sys_write for write msg on standard output
-        mov     rax, 1	;Load the numero system call for sys_exit in rax register
-        xor     rbx, rbx	;Make XOR op in rbx register
-        int	0x80	;System call of sys_exit for end program
-message:		;Start of data section
-        db      "Hello, World", 10	;db define n bytes in ram with newline (10)
+global  main
+section .text
+main:
+        mov     rax, 4
+        mov     rbx, 1
+        mov     rcx, message
+        mov     rdx, 17
+        int	0x80
+
+        mov     rax, 1
+        xor     rbx, rbx
+        int	0x80
+message:
+        db      "Hello, Holberton", 10
