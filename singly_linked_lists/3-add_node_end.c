72e2ce9e0069680ca88b16e69517dd4d005b3ebe
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-02 12:51:21 +0000

    Requirements issue

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,8 +13,14 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	unsigned int len = 0;
 
+	list_t *new_node, *temp;
+
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -25,26 +31,24 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		free(new_node);
 		return (NULL);
+
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (new_node);
 	}
 
-	else
+	temp = *head;
+	while (temp->next != NULL)
 	{
-		list_t *current = *head;
-
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-		current->next = new_node;
+		temp = temp->next;
 	}
 
+	temp->next = new_node;
 	return (new_node);
 }
