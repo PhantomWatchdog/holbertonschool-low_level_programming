687c8bfe2daba7cddd74fd64a368ce2f8154ba3a
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-03 08:49:29 +0000

    Exe deletion and add task 5 file

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - 
+ * @head:
+ * @index:
+ * Return:
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int count = 0;
+
+	while (current != NULL)
+	{
+		if (count == index)
+		{
+			return (current);
+		}
+
+		count++;
+		current = current->next;
+	}
+	
+	return (NULL);
+}
