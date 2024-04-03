f32e1e209d96375aec9b55214f1484b76287f5a8
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-03 07:25:36 +0000

    Test on Task2 and create of a function adding new node at end of dlist.

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,23 +8,23 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL)
-	{
+	if (new == NULL)
+
 		return NULL;
-	}
 
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
 
 	if (*head != NULL)
-	{
-		(*head)->prev = new_node;
-	}
 
-	*head = new_node;
+		(*head)->prev = new;
+
+
+	*head = new;
 	
-	return new_node;
+	return new;
 }
