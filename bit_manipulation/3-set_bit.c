a41b97f4d5975f648a603424ddb30520b8d0dc14
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-04 13:15:25 +0000

    Set value of bit to 1 with given index

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - Set bit value to 1 for specified index.
+ * @n: The bit.
+ * @index: The target index to reach.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	/*XOR operation comparing two bits*/
+	*n ^= (1 << index);
+	return (1);
+}
